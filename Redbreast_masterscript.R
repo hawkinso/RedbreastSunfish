e7d0140b141ba77a1cf59884cd357db2f149911b
ohawkins1 <ohawkins.research@gmail.com>
2021-11-23 13:08:40 -0600

    Started script.
    Included git hub link and prepared data for reading in

---
+++ Redbreast_masterscript.R
@@ -0,0 +1,13 @@
+# Rebreast project 2021 
+# Date: 11/23/2021 
+# Author(s): Olivia H Hawkins 
+# Goals: clean and summarize data,statistical assumptions, transformations 
+
+# Git hub repository 
+browseURL("https://github.com/hawkinso/RedbreastSunfish.git")
+
+# Read in data 
+data <- read.csv("")
+
+# Subset data that will be used 
+# 
